f8663227fc097602d4e48760680dc2ffddd220a9
Damir Kadyrzhanov <damirk120404@gmail.com>
2024-02-16 17:01:05 +0600

    Developer project finder is done

--- src/main/java/com/epam/mjc/collections/combined/DeveloperProjectFinder.java
+++ src/main/java/com/epam/mjc/collections/combined/DeveloperProjectFinder.java
@@ -1,10 +1,21 @@
 package com.epam.mjc.collections.combined;
 
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
 public class DeveloperProjectFinder {
     public List<String> findDeveloperProject(Map<String, Set<String>> projects, String developer) {
+        List<String> result = new ArrayList<>();
+        for(Map.Entry<String, Set<String>> e : projects.entrySet()){
+            if(e.getValue().contains(developer)){
+                result.add(e.getKey());
+            }
+        }
+        result.sort((o1, o2) -> {
+            if(o1.length() == o2.length()){
+                return o2.compareTo(o1);
+            }
+            return o2.length() - o1.length();
+        });
+        return result;
     }
 }
